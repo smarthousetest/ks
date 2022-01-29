b94b593c91a443579177416deb27a85bfa77b346
ramsaf <ramsaf.work@yandex.ru>
2022-01-29 22:53:21 +0300

    Локализация

---
+++ lib/utils/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const LargeTextSize = 24.0;
+const MediumTextSize = 18.0;
+const BodyTextSize = 14.0;
+
+const String FontFamilyText = 'Ubuntu';
+
+Color kPrimaryColor = Color(0xFF2E8B90);
+Color kBacColor = Color(0xFFF6F9F9);
---
+++ lib/utils/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+ThemeData theme() => ThemeData(
+    brightness: Brightness.light,
+    primaryColor: kPrimaryColor,
+    scaffoldBackgroundColor: kBacColor,
+    fontFamily: FontFamilyText);
